b09214d0ae6abe2bef88e04a139f9e977617b2b3
dyshn <dyshn@DESKTOP-KKSH6NU>
2023-12-20 15:37:12 +0100

    Add project files.

---
+++ MathLib/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
